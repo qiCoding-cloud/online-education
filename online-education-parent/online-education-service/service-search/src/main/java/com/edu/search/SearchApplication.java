3ed0ddc5f6b5f5a08771be7eed9d4374a2f4c2de
qitianfeng <1454429051@qq.com>
2020-10-24 09:16:39 +0800

    update

---
+++ online-education-parent/online-education-service/service-search/src/main/java/com/edu/search/SearchApplication.java
@@ -0,0 +1,19 @@
+package com.edu.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@EnableDiscoveryClient
+@EnableFeignClients
+@ComponentScan(basePackages = {"com.edu"})
+public class SearchApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(SearchApplication.class,args);
+    }
+}
